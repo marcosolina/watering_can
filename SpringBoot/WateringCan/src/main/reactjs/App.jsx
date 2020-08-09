05d8b1f959c5dc0fb5bcf689be837d0b8c837c64
Marco <marcosolina@gmail.com>
2020-08-09 16:28:44 +0200

    Adding slider for the ml

--- SpringBoot/WateringCan/src/main/reactjs/App.jsx
+++ SpringBoot/WateringCan/src/main/reactjs/App.jsx
@@ -2,10 +2,6 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Pump from './components/Pump.jsx';
 import { doHttpRequest } from './utils/UtilsFunctions.jsx';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
 import Grid from '@material-ui/core/Grid';
 import InputText from './components/InputText.jsx';
 import Fab from '@material-ui/core/Fab';
@@ -13,6 +9,10 @@ import SaveIcon from '@material-ui/icons/Save';
 import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
+import MlSlider from './components/MlSlider.jsx';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
 
 class App extends Component{
 	constructor(props) {
@@ -91,6 +91,10 @@ class App extends Component{
 		this.setState({snackOpen: false, snackMessage: ""});
 	}
 
+	onChangeSlider(){
+		
+	}
+
 
 	render(){
 
@@ -98,34 +102,38 @@ class App extends Component{
 
 		return(
 				<Grid container spacing={3}>
-					<Grid item xs={12} sm={6} md={4} lg={2}>
-						<List>
-							{
-								pumps.map((pump, index) => {
-									return <ListItem key={index}>
-											<ListItemText>
+					{
+						pumps.map((pump, index) => {
+							return  <Grid item xs={12} sm={6} md={4} lg={3} xl={2} key={index}>
+										<Card>
+											<CardContent>
 												<InputText 
 													name={"" + pump.mac + "_" + pump.id}
 													id={"" + pump.mac + "_" + pump.id}
 													value={pump.description}
 													label={"Plant"}
 													onChange={this.onChangeInput.bind(this)}
-													/>
-											</ListItemText>
-											<ListItemSecondaryAction>
+												/>
+												<br/><br/>
+												<MlSlider 
+													mac={pump.mac}
+													id={pump.id}
+													onChange={this.onChangeSlider.bind(this)}
+												/>
+											</CardContent>
+											<CardActions>
 												<Pump
 													mac={pump.mac}
 													id={pump.id}
 													status={pump.status}
 													onChange={this.onChangePump.bind(this)}
-													/>
-											</ListItemSecondaryAction>
-										</ListItem>
-								})
-							}
-						</List>
-					</Grid>
-					<Grid item>
+												/>
+											</CardActions>
+										</Card>
+									</Grid>
+						})
+					}
+					<Grid item xs={12}>
 						<Fab color="primary" aria-label="save" onClick={this.saveConfig.bind(this)}>
 							<SaveIcon />
 						</Fab>
